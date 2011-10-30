391572ef90d273bc63ea03bff7410e1d457bb224
Thomas Skardal <thomas.skardal@gmail.com>
2011-10-30 19:58:34 +0100

    Added some jquery ajax magic

--- src/main/scala/no/java/ldapadmin/LdapAdminServlet.scala
+++ src/main/scala/no/java/ldapadmin/LdapAdminServlet.scala
@@ -13,6 +13,46 @@ import java.sql.{Timestamp, DriverManager}
 
 class LdapAdminServlet extends ScalatraServlet with ScalateSupport with UrlSupport with FakeMailSender {
 
+  val temporaryUserServiceStub = new UserService {
+      def setPassword(p1: String, p2: String) {}
+
+      def saveUser(p1: User) {
+        println("user saved")
+      }
+
+      def getUserByMail(p1: String) = null
+
+      def findGroupsByMember(p1: String) = null
+
+      def createUser(p1: User) = null
+
+      def resetPassword(p1: String) = ""
+
+      def getUser(p1: String): User = {
+        val user = new User()
+        user.setUid("user.name")
+        user
+      }
+
+      def getDnByUid(p1: String) = ""
+
+      def getUsers = null
+
+      def findUserByMail(p1: String): User = {
+        val user = new User()
+        user.setUid("user.name")
+        user
+      }
+
+      def deleteUser(p1: String) {}
+
+      def search(p1: String) = null
+
+      def saveGroups(p1: String, p2: List[String]) {}
+
+      def getUserByDn(p1: String) = null
+    }
+
   Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
   SessionFactory.concreteFactory = Some(() =>
     Session.create(
@@ -41,57 +81,26 @@ class LdapAdminServlet extends ScalatraServlet with ScalateSupport with UrlSuppo
     }
   }
 
-  get("/reset-password/:username/:identifier") {
-    if (validResetRequest(params("username"), params("identifier"))) {
-      showView("reset-password")
+  get("/reset-password/:userid/:identifier") {
+    if (validResetRequest(params("userid"), params("identifier"))) {
+      showView("reset-password", "userid" -> params("userid"))
     } else {
       showView("recover-password", "errorMessage" -> Some("The reset-link was expired or unknowwn."))
     }
   }
 
-  post("/reset-password") {
-    val email = params("email")
-    //    val userService = new DefaultUserService();
-    //    val user = userService.findUserByMail(email);
-    //    val generatedPassword = userService.resetPassword(user.getUid)
+  post("/reset-password/:userid") {
+    val uid = params("userid")
+    val pwd = params("pwd")
+    val user = temporaryUserServiceStub.getUser(uid);
+    user.setPassword(pwd)
+    temporaryUserServiceStub.saveUser(user)
+
+    println("Password for set to " + pwd + " for user " + uid)
   }
 
   post("/recover-password") {
     val email = params("email")
-    val temporaryUserServiceStub = new UserService {
-      def setPassword(p1: String, p2: String) {}
-
-      def saveUser(p1: User) {}
-
-      def getUserByMail(p1: String) = null
-
-      def findGroupsByMember(p1: String) = null
-
-      def createUser(p1: User) = null
-
-      def resetPassword(p1: String) = ""
-
-      def getUser(p1: String) = null
-
-      def getDnByUid(p1: String) = ""
-
-      def getUsers = null
-
-      def findUserByMail(p1: String): User = {
-        val user = new User()
-        user.setUid("user.name")
-        return user
-      }
-
-      def deleteUser(p1: String) {}
-
-      def search(p1: String) = null
-
-      def saveGroups(p1: String, p2: List[String]) {}
-
-      def getUserByDn(p1: String) = null
-    }
-
     val user = temporaryUserServiceStub.findUserByMail(email)
 
     if (user != null) {
